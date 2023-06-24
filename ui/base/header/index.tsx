8a6c7d1fa713ae20c5c888d72c245ccf36c15000
kaikojima05 <kojimakai5335@gmail.com>
2023-06-25 04:12:05 +0900

    ハンバーガーメニューの開閉をボタンを押さなくても可能にした

--- ui/base/header/index.tsx
+++ ui/base/header/index.tsx
@@ -31,86 +31,97 @@ export default function Header() {
     };
   }, []);
 
-  const toggleHamburgerMenu = () => {
+  const toggleHamburgerMenu = (e: React.MouseEvent<HTMLElement>) => {
     setOpen((prev) => !prev);
+    e.stopPropagation();
   };
 
   return (
-    <nav
-      className={classNames(
-        "z-50 w-full fixed header-border header-blur",
-        `${loading ? "border-animation blur-animation" : ""}`
-      )}
-    >
-      <header
-        className={classNames(
-          "flex justify-between px-4 py-4 elevator",
-          "md:px-28 md:py-6",
-          `${fontColor}`,
-          `${open && "going-down"}`
-        )}
-      >
-        <div>
-          <h1>
-            <Link href="/">porko</Link>
-          </h1>
-        </div>
-        <div className={classNames("hidden", "md:block")}>
-          <ul className={classNames("flex gap-12")}>
-            <li>
-              <Link href="/about/">about</Link>
-            </li>
-            <li>
-              <Link href="/news/">news</Link>
-            </li>
-            <li>
-              <Link href="/contact/">contact</Link>
-            </li>
-          </ul>
-        </div>
+    <>
+      {open && (
         <div
+          className="z-[40] fixed w-screen h-screen"
+          onClick={() => setOpen(false)}
+        />
+      )}
+      <nav
+        className={classNames(
+          "z-50 w-full fixed header-border header-blur",
+          `${loading ? "border-animation blur-animation" : ""}`
+        )}
+        onClick={() => setOpen(false)}
+      >
+        <header
           className={classNames(
-            "hidden",
-            `${open && "!flex items-center justify-center delay-display"}`
+            "flex justify-between px-4 py-4 elevator",
+            "md:px-28 md:py-6",
+            `${fontColor}`,
+            `${open && "going-down"}`
           )}
         >
-          <ul
+          <div onClick={(e) => e.stopPropagation()} className="h-6 w-6">
+            <h1>
+              <Link href="/">porko</Link>
+            </h1>
+          </div>
+          <div className={classNames("hidden", "md:block")}>
+            <ul className={classNames("flex gap-12")}>
+              <li>
+                <Link href="/about/">about</Link>
+              </li>
+              <li>
+                <Link href="/news/">news</Link>
+              </li>
+              <li>
+                <Link href="/contact/">contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div
             className={classNames(
-              " flex flex-col gap-y-6",
-              "[&>]:block [&>li]:text-[1.125rem] [&>li]:text-center [&>li]:border-b [&>li]:border-dotted [&>li]:border-black"
+              "hidden",
+              `${open && "!flex items-center justify-center delay-display"}`
             )}
+            onClick={(e) => e.stopPropagation()}
           >
-            <li>
-              <Link href="/about/">about</Link>
-            </li>
-            <li>
-              <Link href="/news/">news</Link>
-            </li>
-            <li>
-              <Link href="/contact/">contact</Link>
-            </li>
-          </ul>
-        </div>
-        <div
-          className={classNames("hamburger-menu", "md:hidden")}
-          onClick={toggleHamburgerMenu}
-        >
-          <span
-            className={classNames(
-              "hamburger-menu-bar-top",
-              `${hamburgerColor}`,
-              `${open && "top-open"}`
-            )}
-          ></span>
-          <span
-            className={classNames(
-              "hamburger-menu-bar-bottom",
-              `${hamburgerColor}`,
-              `${open && "bottom-open"}`
-            )}
-          ></span>
-        </div>
-      </header>
-    </nav>
+            <ul
+              className={classNames(
+                " flex flex-col gap-y-6",
+                "[&>]:block [&>li]:text-[1.125rem] [&>li]:text-center [&>li]:border-b [&>li]:border-dotted [&>li]:border-black"
+              )}
+            >
+              <li>
+                <Link href="/about/">about</Link>
+              </li>
+              <li>
+                <Link href="/news/">news</Link>
+              </li>
+              <li>
+                <Link href="/contact/">contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div
+            className={classNames("hamburger-menu", "md:hidden")}
+            onClick={toggleHamburgerMenu}
+          >
+            <span
+              className={classNames(
+                "hamburger-menu-bar-top",
+                `${hamburgerColor}`,
+                `${open && "top-open"}`
+              )}
+            ></span>
+            <span
+              className={classNames(
+                "hamburger-menu-bar-bottom",
+                `${hamburgerColor}`,
+                `${open && "bottom-open"}`
+              )}
+            ></span>
+          </div>
+        </header>
+      </nav>
+    </>
   );
 }
