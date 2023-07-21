8cde418bc00ecc594c1e82439fa82dca0e758739
kaikojima <kojimakai5335@gmail.com>
2023-07-22 03:37:35 +0900

    ゴミを削除

--- ui/base/icons/ArrowOneSide.tsx
+++
@@ -1,9 +0,0 @@
-export default function ArrowOneSide() {
-  return (
-    <>
-      <path fillRule="evenodd" clipRule="evenodd" d="M60 11H10V10H60V11Z" fill="currentColor" />
-      <path fillRule="evenodd" clipRule="evenodd" d="M17.7782 3.70635L10.7071 10.7774L10 10.0703L17.0711 2.99924L17.7782 3.70635Z" fill="currentColor" />
-    </>
-  )
-}
-
