f5e109d8bb291bac470a663a01d906023da732b0
kaikojima <kojimakai5335@gmail.com>
2023-07-30 04:09:19 +0900

    hover アニメーションがついた icon を配列で回せるコンポーネントを作成

---
+++ ui/module/hoverIcon/index/index.tsx
@@ -0,0 +1,66 @@
+import classNames from 'classnames'
+import Icon from '@/ui/base/icons/index'
+import type { IconName } from '@/ui/base/icons/index'
+
+type HoverIconsProps = {
+  hrefs: string[]
+  iconsName: IconName[]
+  iconSize: '6' | '7' | '8'
+  iconClipColor: string
+  iconBgColor: string
+  iconHoverBgColor: string
+  iconHoverClipColor: string
+  iconClassName?: string
+}
+
+export default function HoverIcons({
+  hrefs,
+  iconsName,
+  iconSize = '6',
+  iconClipColor,
+  iconBgColor,
+  iconHoverBgColor,
+  iconHoverClipColor,
+  iconClassName = ''
+}: HoverIconsProps) {
+  return (
+    <>
+      {iconsName.map((iconName, index) => (
+        <a
+          key={index}
+          className={classNames(
+            iconClipColor,
+            iconBgColor,
+            iconHoverClipColor,
+            iconHoverBgColor,
+            'rounded-full',
+            iconClassName,
+          )}
+          href={hrefs[index]}
+        >
+          <Icon
+            name={iconName}
+            width={
+              iconSize === '6'
+                ? 'w-6'
+                : iconSize === '7'
+                  ? 'w-7'
+                  : iconSize === '8'
+                    ? 'w-8'
+                    : ''
+            }
+            height={
+              iconSize === '6'
+                ? 'h-6'
+                : iconSize === '7'
+                  ? 'h-7'
+                  : iconSize === '8'
+                    ? 'h-8'
+                    : ''
+            }
+          />
+        </a>
+      ))}
+    </>
+  );
+}
