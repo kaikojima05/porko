98b79a715124597138bda7a3b577ce31b4a037aa
kaikojima <kojimakai5335@gmail.com>
2023-08-10 11:27:17 +0900

    埋め込みカードのスタイルを修正

--- ui/module/cardStyle/index/index.tsx
+++ ui/module/cardStyle/index/index.tsx
@@ -3,8 +3,8 @@ export const CardStyle = () => {
     .e {padding-bottom:  75% !important; border-radius: 4px;}
     .body {box-shadow: none !important;}
     .__wc {border: none; background-color: transparent;}
-    .wc {position: relative; border-bottom-right-radius: 20%;} 
-    .c::before {content: ''; position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-image: url('/images/texture_card.webp'); mix-blend-mode: overlay; opacity: 0.75;}
+    .wc {position: relative; border-bottom-right-radius: 20%; filter: grayscale(20%); opacity: 0.8;}} 
+    .c::before {content: ''; position: absolute; top: 0; left: 0; width: 100%; height: 100%;}
   `
   return cardStyle
 } 
