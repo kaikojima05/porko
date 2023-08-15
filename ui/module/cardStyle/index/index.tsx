d41f3c3eb30786a1de7255ccf797e553e5c016de
kaikojima <kojimakai5335@gmail.com>
2023-08-16 02:07:22 +0900

    add hover animation

--- ui/module/cardStyle/index/index.tsx
+++ ui/module/cardStyle/index/index.tsx
@@ -3,7 +3,7 @@ export const CardStyle = () => {
     .e {padding-bottom:  75% !important; border-radius: 4px;}
     .body {box-shadow: none !important;}
     .__wc {border: none; background-color: transparent;}
-    .wc {position: relative; border-bottom-right-radius: 20%; filter: grayscale(20%); opacity: 0.8;} 
+    .wc {position: relative; border-bottom-right-radius: 20%; filter: grayscale(20%); opacity: 0.8; transition: 0.3s;} 
     .body:hover .wc {filter: grayscale(0%); opacity: 1; transition: 0.3s;}
     .c::before {content: ''; position: absolute; top: 0; left: 0; width: 100%; height: 100%;}
   `
