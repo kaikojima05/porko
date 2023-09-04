b616e5578294dfd7320fcadc0647c2f166eb3c50
kaikojima <kojimakai5335@gmail.com>
2023-09-04 14:41:29 +0900

    amended button style

--- ui/base/button/index.tsx
+++ ui/base/button/index.tsx
@@ -24,28 +24,30 @@ export const Button = ({
   children,
 }: ButtonProps) => {
   const rootSize = classNames(
-    size === 'xs' && 'w-[6.25rem]',
-    size === 's' && 'w-[9.375rem] h-[2.5rem]',
-    size === 'm' && 'w-[12.5rem] h-[3.125rem]',
-    size === 'l' && 'w-[15.625rem] h-[3.75rem]',
-    size === 'xl' && 'w-[18.75rem] h-[4rem]',
-    size === 'x-full' && 'w-full',
-    size === 'y-full' && 'h-full',
-    size === 'all-full' && 'x-full h-full',
-    size === 'custom' && '',
+    size === 'xs' ? 'w-[6.25rem]' :
+      size === 's' ? 'w-[9.375rem] h-[2.5rem]' :
+        size === 'm' ? 'w-[12.5rem] h-[3.125rem]' :
+          size === 'l' ? 'w-[15.625rem] h-[3.75rem]' :
+            size === 'xl' ? 'w-[18.75rem] h-[4rem]' :
+              size === 'x-full' ? 'w-full' :
+                size === 'y-full' ? 'h-full' :
+                  size === 'all-full' ? 'x-full h-full' :
+                    size === 'custom' ? '' :
+                      ''
+
   )
 
   const rootStyle = classNames(
-    style === 'rounded' &&
-    `${rootSize} border rounded-full`,
-    style === 'underBar' &&
-    `${rootSize} border border-b`,
-    style === 'circle' &&
-    'border rounded-[100%]',
-    style === 'square' &&
-    `${rootSize} border`,
-    style === 'none' &&
-    '',
+    style === 'rounded' ?
+      `${rootSize} border rounded-full` :
+      style === 'underBar' ?
+        `${rootSize} border-b` :
+        style === 'circle' ?
+          'border rounded-[100%]' :
+          style === 'square' ?
+            `${rootSize} border` :
+            style === 'none' ? '' :
+              '',
   )
 
   return (
