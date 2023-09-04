4230f2aa2402a04efbab9cea38837e97eda75fdc
kaikojima <kojimakai5335@gmail.com>
2023-09-04 14:59:49 +0900

    added the data

--- ui/pages/works/section/main/index/index.tsx
+++ ui/pages/works/section/main/index/index.tsx
@@ -6,24 +6,27 @@ import CategoryList from '@/ui/base/categoryList'
 import Pagination from '@/ui/base/pagination/index'
 
 type WorksPageProps = {
+  data: Works[]
   works: Works[];
   totalPage: number;
 }
 
 export default function WorksPage({
+  data,
   works,
   totalPage
 }: WorksPageProps) {
   const router = useRouter()
   const pageNumberFromQuery = router.query.number ? Number(router.query.number) : 1
 
+
   return (
     <>
       <Section isTopMargin={false}>
-        <CategoryList works={works} />
+        <CategoryList baseUrl="works" data={data} />
         <AllWorksCard works={works} />
         <Pagination
-          url="works/page/"
+          url="works"
           totalPage={totalPage}
           currentPageNumber={pageNumberFromQuery}
         />
