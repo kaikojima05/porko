ac7b0afc5b5bc3db72054eb68617f44f3282f56b
kaikojima <kojimakai5335@gmail.com>
2023-09-04 14:46:19 +0900

    amend categoryList

--- ui/base/categoryList/index.tsx
+++ ui/base/categoryList/index.tsx
@@ -1,24 +1,28 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import classNames from 'classnames'
-import type { Works } from '@/ui/base/types/works'
-import type { Creatives } from '@/ui/base/types/creatives'
-import { Button } from '@/ui/base/button'
+import { useRouter } from "next/router";
+import Link from "next/link";
+import classNames from "classnames";
+import type { Works } from "@/ui/base/types/works";
+import type { Creatives } from "@/ui/base/types/creatives";
+import { Button } from "@/ui/base/button";
 
 type CategoryListProps = {
-  baseUrl: 'works' | 'creatives'
-  data: Works[] | Creatives[] | undefined
-}
+  baseUrl: "works" | "creatives";
+  data: Works[] | Creatives[] | undefined;
+};
 
 export default function CategoryList({ baseUrl, data }: CategoryListProps) {
-  const router = useRouter()
-  const path = router.asPath
-  const currentPath = decodeURIComponent(path)
-  const categoryData = data?.map((item) => item.category)
-  const categories = Array.from(new Set(categoryData?.flat()))
+  const router = useRouter();
+  const path = router.asPath;
+  const currentPath = decodeURIComponent(path);
+  const categoryData = data?.map((item) => item.category);
+  const categories = Array.from(new Set(categoryData?.flat()));
 
   return (
-    <div className={classNames('lg:flex lg:justify-center lg:items-center lg:gap-4')}>
+    <div
+      className={classNames(
+        "lg:flex lg:justify-center lg:items-center lg:gap-4"
+      )}
+    >
       {categories &&
         categories.map((category, index) => (
           <Button
@@ -26,24 +30,25 @@ export default function CategoryList({ baseUrl, data }: CategoryListProps) {
             size="s"
             style="square"
             optionClassName={classNames(
-              'rounded',
-              `${currentPath.includes(category) ? 'bg-primary text-white' : 'border-base-black'}`
+              "rounded",
+              `${
+                currentPath.includes(category)
+                  ? "bg-primary text-white"
+                  : "border-base-black"
+              }`
             )}
           >
             <Link
               className={classNames(
-                'flex justify-center items-center w-full h-full',
+                "flex justify-center items-center w-full h-full"
               )}
-              href={
-                `/${baseUrl}/${category}/`
-              }
+              href={`/${baseUrl}/${category}/`}
               scroll={false}
             >
               {category}
             </Link>
           </Button>
-        ))
-      }
+        ))}
     </div>
   );
 }
