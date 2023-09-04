2461e19e3a0f8e4197cfe90c500c7ce339feca43
kaikojima <kojimakai5335@gmail.com>
2023-09-04 14:58:23 +0900

    amended card style

--- ui/module/cardStyle/index/index.tsx
+++ ui/module/cardStyle/index/index.tsx
@@ -3,9 +3,8 @@ export const CardStyle = () => {
     .e {padding-bottom:  75% !important; border-radius: 4px;}
     .body {box-shadow: none !important;}
     .__wc {border: none; background-color: transparent;}
-    .wc {position: relative; border-bottom-right-radius: 20%; filter: grayscale(20%); opacity: 0.8; transition: 0.3s;} 
+    .wc {position: relative; z-index: 10; border-bottom-right-radius: 20%; filter: grayscale(20%); opacity: 0.8; transition: 0.3s;} 
     .body:hover .wc {filter: grayscale(0%); opacity: 1; transition: 0.3s;}
-    .c::before {content: ''; position: absolute; top: 0; left: 0; width: 100%; height: 100%;}
   `
   return cardStyle
 } 
