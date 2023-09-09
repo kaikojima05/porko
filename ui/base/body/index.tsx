0143ba3d43750cd223332776694b1334cac34756
kaikojima <kojimakai5335@gmail.com>
2023-09-10 07:06:43 +0900

    cta 修正

--- ui/base/body/index.tsx
+++ ui/base/body/index.tsx
@@ -1,20 +1,20 @@
-import { motion } from 'framer-motion'
-import { useRef, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { motion } from "framer-motion";
+import { useRef, useEffect } from "react";
+import { useRouter } from "next/router";
 import classNames from "classnames";
 import Article from "@/ui/base/article";
-import Section from '@/ui/base/section'
+import Section from "@/ui/base/section";
 import Header from "@/ui/base/header";
 import Footer from "@/ui/base/footer";
-import Cta from '@/ui/base/cta/index'
-import Image from 'next/image'
-import { HeadingH1, HeadingH2, HeadingH3 } from '@/ui/base/heading/index'
-import { useOnScrollAnimation } from '@/ui/hooks/useOnScrollAnimation'
+import Cta from "@/ui/base/cta/index";
+import Image from "next/image";
+import { HeadingH1, HeadingH2, HeadingH3 } from "@/ui/base/heading/index";
+import { useOnScrollAnimation } from "@/ui/hooks/useOnScrollAnimation";
 
 type BodyProps = {
   whats: React.ReactNode;
-  src: string
-  heading?: string
+  src: string;
+  heading?: string;
   children?: React.ReactNode;
   bodyClassName?: string;
 };
@@ -24,30 +24,29 @@ export default function Body({
   src,
   heading,
   children,
-  bodyClassName = '',
+  bodyClassName = "",
 }: BodyProps) {
-  const pageTitleRef = useRef<HTMLHeadingElement | null>(null)
-  const router = useRouter()
-  const backgroundRef = useOnScrollAnimation()
+  const pageTitleRef = useRef<HTMLHeadingElement | null>(null);
+  const router = useRouter();
+  const backgroundRef = useOnScrollAnimation();
 
   useEffect(() => {
     if (!pageTitleRef.current) return;
 
     let currentIndex = 0;
 
-    const spans = pageTitleRef.current.querySelectorAll('span')
+    const spans = pageTitleRef.current.querySelectorAll("span");
 
     const animationInterval = setInterval(() => {
       if (currentIndex < spans.length) {
-        spans[currentIndex].classList.remove('opacity-0')
-        currentIndex += 1
+        spans[currentIndex].classList.remove("opacity-0");
+        currentIndex += 1;
       } else {
-        clearInterval(animationInterval)
+        clearInterval(animationInterval);
       }
-    }, 170)
+    }, 170);
 
-    return () => clearInterval(animationInterval)
-
+    return () => clearInterval(animationInterval);
   }, []);
 
   return (
@@ -58,23 +57,23 @@ export default function Body({
       transition={{ duration: 1 }}
     >
       <Header />
-      <div className={classNames(
-        'pt-[3.5625rem] w-full',
-        'lg:pt-[5.1875rem]',
-      )}>
-        <div ref={backgroundRef} className={classNames(
-          "before-scroll-once h-[18rem] w-full",
-          "md:h-[21rem]",
-          "lg:h-[25rem]"
-        )}>
+      <div className={classNames("pt-[3.5625rem] w-full", "lg:pt-[5.1875rem]")}>
+        <div
+          ref={backgroundRef}
+          className={classNames(
+            "before-scroll-once h-[18rem] w-full",
+            "md:h-[21rem]",
+            "lg:h-[25rem]"
+          )}
+        >
           <Image
             alt="portforio サイト porko のイメージ画像"
             src={src}
             fill
             style={{
-              userSelect: 'none',
-              objectFit: 'cover',
-              objectPosition: 'center',
+              userSelect: "none",
+              objectFit: "cover",
+              objectPosition: "center",
             }}
           />
         </div>
@@ -89,77 +88,79 @@ export default function Body({
             `${bodyClassName}`
           )}
         >
-          <div className={classNames(
-            'hidden',
-            'xl:inline xl:fixed xl:bottom-8 xl:left-4 xl:z-10',
-          )}
+          <div
+            className={classNames(
+              "hidden",
+              "xl:inline xl:fixed xl:bottom-8 xl:left-4 xl:z-10"
+            )}
           >
-            <HeadingH1 headingClassName='writing-vertical !text-[1.8125rem]' ref={pageTitleRef}>
+            <HeadingH1
+              headingClassName="writing-vertical !text-[1.8125rem]"
+              ref={pageTitleRef}
+            >
               {whats}
             </HeadingH1>
           </div>
-          <div className={classNames(
-            'w-full',
-            'lg:min-w-[58.125rem] lg:max-w-[58.125rem]',
-            'xl:min-w-[62.5rem] xl:max-w-[62.5rem]'
-          )}>
-            <Article articleClassName='font-content'>
-              {heading &&
+          <div
+            className={classNames(
+              "w-full",
+              "lg:min-w-[58.125rem] lg:max-w-[58.125rem]",
+              "xl:min-w-[62.5rem] xl:max-w-[62.5rem]"
+            )}
+          >
+            <Article articleClassName="font-content">
+              {heading && (
                 <Section isTopMargin={true}>
                   <div className="flex items-center">
-                    <div className='w-full h-[1px] bg-base-black' />
-                    <HeadingH2 outsideClassName={classNames(
-                      'px-4',
-                      'md:px-6',
-                      'lg:px-8'
-                    )}>
+                    <div className="w-full h-[1px] bg-base-black" />
+                    <HeadingH2
+                      outsideClassName={classNames(
+                        "px-4",
+                        "md:px-6",
+                        "lg:px-8"
+                      )}
+                    >
                       {heading}
                     </HeadingH2>
-                    <div className='w-full h-[1px] bg-base-black' />
+                    <div className="w-full h-[1px] bg-base-black" />
                   </div>
                 </Section>
-              }
+              )}
               {children}
             </Article>
           </div>
-          {router.pathname !== '/contact' &&
-            <div className={classNames(
-              'w-screen relative h-[12.5rem] mt-[3.125rem] bg-secondary',
-              "md:h-[16.875rem] mt-[4.275rem]",
-              "lg:h-[20.625rem] lg:mt-[5.625rem]",
-              "xl:h-[23.125rem]"
-            )}>
-              <div className='absolute w-full top-[50%] translate-y-[-50%]'>
-                <Cta
-                  cta={
-                    <>
-                      <HeadingH2
-                        headingClassName={classNames(
-                          '!text-[1.25rem] text-base-black',
-                          'md:!text-[1.5rem]',
-                          'lg:!text-[1.75rem]',
-                        )}
-                      >
-                        contact
-                      </HeadingH2>
-                      <HeadingH3
-                        headingClassName={classNames(
-                          'mt-4 text-base-black',
-                          'md:mt-[1.25rem]',
-                          'lg:mt-[1.875rem]'
-                        )}
-                      >
-                        お問い合わせはこちらから
-                      </HeadingH3>
-                    </>
-                  }
-                  ctaStyle=""
-                >
-                  mail form
-                </Cta>
+          {router.pathname !== "/contact" &&
+            router.pathname !== "/contact/success" && (
+              <div
+                className={classNames(
+                  "w-screen relative h-[12.5rem] mt-[3.125rem] bg-secondary",
+                  "md:h-[16.875rem] mt-[4.275rem]",
+                  "lg:h-[20.625rem] lg:mt-[5.625rem]",
+                  "xl:h-[23.125rem]"
+                )}
+              >
+                <div className="absolute w-full top-[50%] translate-y-[-50%]">
+                  <Cta
+                    cta={
+                      <>
+                        <HeadingH2
+                          headingClassName={classNames(
+                            "!text-[1.25rem] text-base-black",
+                            "md:!text-[1.5rem]",
+                            "lg:!text-[1.75rem]"
+                          )}
+                        >
+                          contact
+                        </HeadingH2>
+                      </>
+                    }
+                    ctaStyle=""
+                  >
+                    お問い合わせはこちらから
+                  </Cta>
+                </div>
               </div>
-            </div>
-          }
+            )}
         </main>
       </div>
       <Footer />
