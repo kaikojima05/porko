bc99dedae432b49b6cccb8ad734d0eafbd59b0fa
kaikojima <kojimakai5335@gmail.com>
2023-09-10 07:07:42 +0900

    form の構造修正

--- ui/pages/contact/section/main/index/index.tsx
+++ ui/pages/contact/section/main/index/index.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import emailjs from 'emailjs-com'
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { set, useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
 import classNames from "classnames";
 import Section from "@/ui/base/section";
-import { Button } from '@/ui/base/button'
-import FormInput from '@/ui/snippet/formInput'
-import { HeadingH3 } from '@/ui/base/heading'
+import FormInput from "@/ui/snippet/formInput";
 
 type FormData = {
   name: string;
@@ -15,107 +14,131 @@ type FormData = {
 };
 
 export default function ContactPage() {
-  const [name, setName] = useState("")
-  const [company, setCompany] = useState("")
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
+    handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
 
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit = async (data: FormData) => {
+    setIsLoading(true);
 
     const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
     const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
     const publicId = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_ID!;
 
     const templateParams = {
-      name: name,
-      company: company,
-      email: email,
-      message: message,
-    }
+      name: data.name,
+      company: data.company,
+      email: data.email,
+      message: data.message,
+    };
 
     try {
       await emailjs.send(serviceId, templateId, templateParams, publicId);
+      router.push("/contact/success");
     } catch (error) {
-      console.error("エラーが出ました" + error)
+      console.error("エラーが出ました" + error);
     }
-  }
+
+    setIsLoading(false);
+  };
   return (
     <>
       <Section isTopMargin={false}>
         <div>
-          <h3 className={classNames(
-            'text-[0.75rem] text-center',
-            'lg:text-[0.875rem]'
-          )}>
+          <h3
+            className={classNames(
+              "text-[0.75rem] text-center",
+              "lg:text-[0.875rem]"
+            )}
+          >
             お仕事に関するご相談や、その他ご不明点など
-            <br className='md:hidden' />
+            <br className="md:hidden" />
             下記フォームにご入力の上、送信してください。
             <br />
             ご連絡お待ちしております。
           </h3>
         </div>
-        <form onSubmit={onSubmit} className={classNames(
-          'mt-[3.75rem]',
-        )}>
-          <div className={classNames(
-            "[&>*]:mt-4",
-            "first:[&>*]:mt-0",
-          )}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={classNames(
+            "mt-[1.875rem]",
+            "md:mt-[2.8125rem]",
+            "lg:mt-[3.75rem]"
+          )}
+        >
+          <div className={classNames("[&>*]:mt-4", "first:[&>*]:mt-0")}>
             <FormInput
-              forName='name'
-              type='text'
-              placeholder='your name'
-              register={register('name', { required: true })}
-              onChange={(event) => setName(event.target.value)}
-              value={name}
+              forName="name"
+              type="text"
+              placeholder="your name"
+              register={register("name", {
+                required: "! 氏名を入力してください",
+              })}
             />
+            <p className="text-primary">{errors.name?.message}</p>
             <FormInput
-              forName='company'
-              type='text'
-              placeholder='your company'
-              register={register('company', { required: false })}
-              onChange={(event) => setCompany(event.target.value)}
-              value={company}
+              forName="company"
+              type="text"
+              placeholder="your company"
+              register={register("company", {
+                required: "! 企業名を入力してください",
+              })}
             />
+            <p className="text-primary">{errors.company?.message}</p>
             <FormInput
-              forName='email'
-              type='text'
-              placeholder='your email address'
-              register={register('email', { required: true })}
-              onChange={(event) => setEmail(event.target.value)}
-              value={email}
+              forName="email"
+              type="text"
+              placeholder="your email address"
+              register={register("email", {
+                required: "! メールアドレスを入力してください",
+              })}
             />
+            <p className="text-primary">{errors.email?.message}</p>
             <FormInput
-              forName='message'
-              type='textarea'
-              placeholder='please free message'
-              register={register('message', { required: true })}
-              onChange={(event) => setMessage(event.target.value)}
-              value={message}
+              forName="message"
+              type="textarea"
+              placeholder="please free message"
+              register={register("message", {
+                required: "! お問い合わせ内容を入力してください",
+              })}
             />
+            <p className="text-primary">{errors.message?.message}</p>
           </div>
-          <div className="mt-8">
-            <Button
-              size="m"
-              style="square"
-              optionClassName={classNames(
-                "ml-auto border-base-black rounded duration-300",
-                "lg:hover:bg-base-black lg:hover:text-white"
-              )}
-            >
-              <button type='submit'>
-                send message
-              </button>
-            </Button>
+          <div
+            className={classNames(
+              "mt-[1.875rem]",
+              "md:mt-[2.8125rem]",
+              "lg:mt-[3.75rem]"
+            )}
+          >
+            <div className="flex justify-end">
+              <div
+                className={classNames(
+                  "inline-block rounded-full border border-base-black duration-300",
+                  "lg:hover:bg-primary lg:hover:text-white lg:hover:border-primary"
+                )}
+              >
+                <button
+                  className={classNames(
+                    "px-4 py-1 ring-0",
+                    "md:px-6 md:py-2",
+                    "lg:px-8 lg:py-3"
+                  )}
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  send message
+                </button>
+              </div>
+            </div>
           </div>
         </form>
       </Section>
     </>
-  )
+  );
 }
--- ui/snippet/formInput.tsx
+++ ui/snippet/formInput.tsx
@@ -1,15 +1,17 @@
-import { useRef } from 'react'
-import classNames from 'classnames'
-import { UseFormRegisterReturn } from 'react-hook-form'
+import { useRef } from "react";
+import classNames from "classnames";
+import { UseFormRegisterReturn } from "react-hook-form";
 
 type FormInputProps = {
-  register: UseFormRegisterReturn
-  forName: string
-  type: string
-  placeholder: string
-  onChange?: (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  register: UseFormRegisterReturn;
+  forName: string;
+  type: string;
+  placeholder: string;
+  onChange?: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
   value?: string;
-}
+};
 
 export default function FormInput({
   register,
@@ -19,52 +21,52 @@ export default function FormInput({
   onChange,
   value,
 }: FormInputProps) {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleClickInputArea = () => {
     if (inputRef.current) {
-      inputRef.current.focus()
+      inputRef.current.focus();
     } else if (textareaRef.current) {
-      textareaRef.current.focus()
+      textareaRef.current.focus();
     }
-  }
+  };
 
   return (
-    <div className={classNames(
-      `${type !== 'textarea' ? 'flex' : ''}`,
-      'border border-base-black px-3 py-2 rounded',
-      'md:px-7 md:py-4',
-      'lg:px-10 lg:py-6'
-    )}
+    <div
+      className={classNames(
+        `${type !== "textarea" ? "flex" : ""}`,
+        "border border-base-black px-3 py-2 rounded",
+        "md:px-7 md:py-4",
+        "lg:px-10 lg:py-6"
+      )}
       onClick={handleClickInputArea}
     >
-      <div className='basis-1/5'>
+      <div className="basis-1/5">
         <label
           htmlFor={forName}
           className={classNames(
             "text-[0.875rem] mr-3",
             "md:mr-0",
-            "lg:px-2 lg:border-b lg:border-dashed lg:border-base-black lg:pb-1 lg:px-1",
+            "lg:px-2 lg:border-b lg:border-dashed lg:border-base-black lg:pb-1"
           )}
         >
           {forName}
         </label>
       </div>
-      <div className={classNames(
-        "w-full grow-[2] text-[0.875rem] ",
-        "[&>*]:w-full [&>*]:outline-none [&>*]:bg-[url('/images/texture.webp')]"
-      )}>
-        {type !== 'textarea' ? (
+      <div
+        className={classNames(
+          "w-full grow-[2] text-[0.875rem] ",
+          "[&>*]:w-full [&>*]:outline-none [&>*]:bg-[url('/images/texture.webp')]"
+        )}
+      >
+        {type !== "textarea" ? (
           <input
             id={forName}
             type={type}
             {...register}
             placeholder={placeholder}
-            ref={inputRef}
             name={forName}
-            onChange={onChange}
-            value={value}
           />
         ) : (
           <textarea
@@ -76,12 +78,10 @@ export default function FormInput({
             {...register}
             placeholder={placeholder}
             rows={10}
-            ref={textareaRef}
-            onChange={onChange}
-            value={value}
+            name={forName}
           />
         )}
       </div>
-    </div >
-  )
+    </div>
+  );
 }
