ac1d072a74de02701656fb0f642f4bc27a5f4f26
kaikojima <kojimakai5335@gmail.com>
2023-09-10 07:07:02 +0900

    class のゴミ削除

--- ui/base/cta/index/index.tsx
+++ ui/base/cta/index/index.tsx
@@ -1,36 +1,32 @@
-import classNames from 'classnames';
-import Link from 'next/link';
+import classNames from "classnames";
+import Link from "next/link";
 
 type CtaProps = {
-  cta: React.ReactNode
-  ctaStyle?: string
-  children: React.ReactNode
-}
+  cta: React.ReactNode;
+  ctaStyle?: string;
+  children: React.ReactNode;
+};
 
-export default function Cta({
-  cta,
-  ctaStyle,
-  children,
-}: CtaProps) {
+export default function Cta({ cta, ctaStyle, children }: CtaProps) {
   return (
-    <div className={classNames(
-      'w-full',
-      `${ctaStyle}`
-    )}>
-      <div className='text-center'>
-        {cta}
-      </div>
-      <div className={classNames(
-        'mt-4 flex items-center justify-center',
-        'md:mt-[1.25rem]',
-        'lg:mt-[1.875rem]'
-      )}>
-        <Link href="/contact/" className={classNames(
-          'text-[1.125rem] flex justify-center items-center px-3 border-b border-primary-light border-b-[0.75rem] leading-[0.6rem] text-base-black',
-          'md:text-[1.375rem]',
-          'lg:text-[1.75rem]',
+    <div className={classNames("w-full", `${ctaStyle}`)}>
+      <div className="text-center">{cta}</div>
+      <div
+        className={classNames(
+          "mt-4 flex items-center justify-center",
+          "md:mt-[1.25rem]",
+          "lg:mt-[1.875rem]"
         )}
-          scroll={false}>
+      >
+        <Link
+          href="/contact/"
+          className={classNames(
+            "text-[1.125rem] flex justify-center items-center px-3 border-primary-light border-b-[0.75rem] leading-[0.6rem] text-base-black",
+            "md:text-[1.375rem]",
+            "lg:text-[1.75rem]"
+          )}
+          scroll={false}
+        >
           {children}
         </Link>
       </div>
