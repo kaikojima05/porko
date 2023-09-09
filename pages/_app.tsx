82407086d6083d0f7bd3762d920b52b9b41f6420
kaikojima <kojimakai5335@gmail.com>
2023-09-10 08:53:23 +0900

    DefaultSeo を設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { DefaultSeo } from "next-seo";
 import { AnimatePresence } from "framer-motion";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -5,6 +6,33 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
+      <DefaultSeo
+        title="porko"
+        description="東京・八王子を拠点に活動するフリーランスライターのポートフォリオサイト。Webコンテンツの執筆を始め、「ことば」で表現するお仕事に幅広く対応させていただきます。"
+        openGraph={{
+          type: "website",
+          title: "porko",
+          description:
+            "東京・八王子を拠点に活動するフリーランスライターのポートフォリオサイト。Webコンテンツの執筆を始め、「ことば」で表現するお仕事に幅広く対応させていただきます。",
+          site_name: "フリーランスライター小嶋麻莉恵のポートフォリオサイト",
+          url: "https://porko.jp",
+          images: [
+            {
+              url: "/images/hero_top.webp",
+              width: 800,
+              height: 600,
+              alt: "",
+              type: "image/webp",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "こじまり｜ライター",
+          site: "@koji_mari7",
+          cardType: "summary_large_image",
+        }}
+      />
+
       <Component key={router.asPath} {...pageProps} />
     </AnimatePresence>
   );
